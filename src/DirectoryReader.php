8c7e24e9c861bbb615d80034c9d54d44320a6a6a
fabiosan75 <fabiosan75@gmail.com>
2022-03-31 15:18:48 -0500

    Implementacion Funcional V1 DirectoryReader

--- src/DirectoryReader.php
+++ src/DirectoryReader.php
@@ -1,107 +1,153 @@
 <?php
-
+/**
+ * PHP version 7
+ *
+ * @category Class
+ * @package  GetTreeRepository
+ * @author   fabiosan75 <fabiosan75@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/fabiosan75
+ */
 namespace GetTreeRepository;
 
-require_once __DIR__.'/../vendor/autoload.php';
-
 use GetTreeRepository\Interfaces\DirectoryReaderInterface;
-use RuntimeException;
+use GetTreeRepository\Util\FileSystemException;
 
 /**
-* FileReader : Implementa los metodos para el acceso al FileSystem
-* 
-* @category Class
-* @package  GetTreeRepository
-* @author   fabiosan75 <fabiosan75@gmail.com>
-* @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
-* @link     https://github.com/fabiosan75
-*/
-class DirectoryReader implements DirectoryReaderInterface 
+ * DirectoryReader : Implementa los metodos para el acceso al FileSystem y
+ *                   obtener lista de archivos 
+ * 
+ * @category Class
+ * @package  GetTreeRepository
+ * @author   fabiosan75 <fabiosan75@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/fabiosan75
+ */
+
+class DirectoryReader implements DirectoryReaderInterface
 {
     
-    protected $_path;
+    protected $path;
     
     /**
-    * Method __construct
-    *
-    * @param string $dirPath [explicite description]
-    *
-    * @return void
-    */
+     * Method __construct
+     *
+     * @param string $dirPath src / PATH al directorio a leer
+     *
+     * @return void
+     */
     public function __construct(string $dirPath)
-    {
-        $this->path = $dirPath;       
+    {  
+        try {
+            if (!$this->isDirectory($dirPath)) {
+                throw new FileSystemException('Directorio '.$dirPath.' No Valido');
+            } else {
+                $this->path = $dirPath;
+            }
+        
+        } catch (FileSystemException $e) {
+            $e->fsError();
+        }
     }
     
     /**
-    * Method getPath
-    *
-    * @return string
-    */
+     * Method getPath   Retorn src/Path al directorio instanciado en la clase
+     *
+     * @return string
+     */
     public function getPath():string
     {
-        return $this->_path;       
+        return $this->path;       
     }
     
     /**
-    * Method listDirectory Retorna los direcotrios de un repositorio 
-    * <$dirpath> que contienen un archivo especificado en $pattern
-    * EJ : composer.json  >>  
-    *
-    * @param  string $dirPath Sorce del direcotrio a explorar
-    * @param  string $pattern Patrón para filtrar los directorios listados
-    *                         EJ : </(composer.json)/>
-    * @return array
-    */
-    public function listDir( string $dirPath, string $pattern): array
-    {
-        
-        $files = [];
-        $dir = '';
-        
-      //  if (!is_dir($dirPath)) {
-      //      return 0;
-      //  }
-        
-        if ($dirresource = opendir($dirPath)) {
-            
-            while (($file = readdir($dirresource)) !== false) {
-                if ($file == '.' || $file == '..') {
-                    continue;
-                }
-                
-                $filePath = $dir . '/' . $file;
-                
-                if (is_dir($dirPath)) {
-                    $files = array_merge($files, $this->listDir($filePath, $pattern));
-                } else {
-                    if (preg_match($pattern, $file)) {
-                        $files[basename($dir)] = $file;
-                    }
-                    // array_push($files, $filepath);
-                }
-            }
-            closedir($dirresource);
-            
-        } else {
-            throw new RuntimeException('Erro de lectura DirPath'.$dirPath); 
-        }
-        
-        return $files;
-        
+     * Method isReadable Valida si el $path es un directorio 
+     *
+     * @param $dirPath src/Path ruta del directorio a validar
+     *
+     * @return bool
+     */
+    public function isDirectory(string $dirPath): bool
+    { 
+        return is_dir($dirPath);
     }
     
+    /**
+     * Method listDir Retorna los directorios de un repositorio 
+     *                <$dirpath> que contienen un archivo especificado 
+     *                en $pattern EJ : composer.json  >>  
+     *
+     * @param string $pattern Patrón para filtrar los directorios listados
+     *                        EJ : </(composer.json)/>
+     * 
+     * @return array
+     */
+    public function listDir(string $pattern):array 
+    {
+        $files = [];
+        $fh = opendir($this->path);
+
+        while (($file = readdir($fh)) !== false) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+
+            $filePath = $this->path . '/' . $file;
+
+            if (is_dir($filePath) && file_exists($filePath.'/'.$pattern)) {
+       
+                $files[basename($filePath)] = $filePath.'/'.$pattern;
+            }
+        }
+
+        closedir($fh);
+        return $files;
+    }
+    
+    /**
+     * Method show Devuelve una cadena formateada con la lista de directorios
+     * y archivos contenidos en $listFiles
+     * 
+     *  Ej :    GetTreeRepository =>  ..//GetTreeRepository/composer.json
+     *                  Proyecto1 =>  ..//Proyecto1/composer.json
+     *                  libreria2 =>  ..//libreria2/composer.json
+     *                  libreria4 =>  ..//libreria4/composer.json
+     *                  Proyecto2 =>  ..//Proyecto2/composer.json
+     *
+     * @param array $listFiles Array("Dir"=>"FileName")
+     *
+     * @return string
+     */
+    public function show(array $listFiles): string
+    {
+        $strLine = '';
+
+        foreach ($listFiles AS $dirName => $file) {
+            $strLine .= sprintf(" %25s =>  %-20s \n", $dirName, $file);
+        }
+
+        return $strLine;
+    }
+
 }
+
+/*
+
+Ejeplo uso Class
 
 $pathDir = $argv[1];
 
 $dirReader = new DirectoryReader($pathDir); 
-$files = $dirReader->listDir($pathDir, '/(composer.json)/');
+$files = $dirReader->listDir('composer.json');
+echo $dirReader->show($files);
 
+**** Listar
 
-print_r($files);
-foreach ($files AS $dirName => $file) {
-    echo $dirName . '=>' . $file . PHP_EOL;
-}
+//foreach ($files AS $dirName => $file) {
+//    echo $dirName . ' => ' . $file . PHP_EOL;
+//}
+
+*/
+
 
 

73d4bebdeb9cd38c873c82132e8a97b871ea7149
fabiosan75 <fabiosan75@gmail.com>
2022-06-09 16:20:41 -0500

    Actualizar Front prueba video

---
+++ FrontAmpliffy/js/video_player.js
@@ -0,0 +1,5 @@
+function show_video_player(){
+    document.getElementById('image_post_video').style.display= 'none';
+    document.getElementById('img_play_youtube').style.display= 'none';
+    document.getElementById('video_player').style.display= 'block';
+}
